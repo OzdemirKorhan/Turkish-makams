0d88471dfd6ef143111a6049e718dd7f6dff9c28
OzdemirKorhan <40806748+OzdemirKorhan@users.noreply.github.com>
2021-05-29 17:25:17 +0300

    Initial commit

---
+++ precache-manifest.a65b5bcd4784dccd18c3ad165a0aff74.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d7d664122cd6454bac",
+    "url": "/css/app.33a44feb.css"
+  },
+  {
+    "revision": "d5b94854302a600e1a014d1fa0ba4a85",
+    "url": "/img/Acemasiran.d5b94854.png"
+  },
+  {
+    "revision": "d18e412cf90faaf7fe84c5de37461c45",
+    "url": "/img/Buselik.d18e412c.png"
+  },
+  {
+    "revision": "5a19b8a019cc49d8eb21d23fbc80bad5",
+    "url": "/img/Cargah.5a19b8a0.png"
+  },
+  {
+    "revision": "6293d06d755210b5ff103d82a007015d",
+    "url": "/img/Evcara.6293d06d.png"
+  },
+  {
+    "revision": "0f2d36ae6075c0d37c7d7ae73672be3a",
+    "url": "/img/Hicaz.0f2d36ae.png"
+  },
+  {
+    "revision": "217f0a0a99908b8e7d3ce8e778df8eb3",
+    "url": "/img/Humayun.217f0a0a.png"
+  },
+  {
+    "revision": "b6d661f197054c56dfe17fb7c5b8c609",
+    "url": "/img/Huseyni.b6d661f1.png"
+  },
+  {
+    "revision": "92c883d67cf468380708cd8b906879f2",
+    "url": "/img/Mahur.92c883d6.png"
+  },
+  {
+    "revision": "4091f3347db840c002dfac4631e529a0",
+    "url": "/img/Neva.4091f334.png"
+  },
+  {
+    "revision": "b354c7d776993d76c4315c77bb743577",
+    "url": "/img/Nihavent.b354c7d7.png"
+  },
+  {
+    "revision": "50ddf26a91c80b91efa8bbbf91ea3c41",
+    "url": "/img/Rast.50ddf26a.png"
+  },
+  {
+    "revision": "4ecf60bea543182e2312d9f1dc6bbfd8",
+    "url": "/img/Ruhnevaz.4ecf60be.png"
+  },
+  {
+    "revision": "e2ffc7a71de40d6b7086f229c87ec94b",
+    "url": "/img/Uzzal.e2ffc7a7.png"
+  },
+  {
+    "revision": "2736c1f83fa7e7b05b381732a5817463",
+    "url": "/img/Uşşak.2736c1f8.png"
+  },
+  {
+    "revision": "719fb64e7aeb8a4220a5dfb16234618e",
+    "url": "/img/Zirguleli-hicaz.719fb64e.png"
+  },
+  {
+    "revision": "1c7daef5d5545cbf5d3629f58c1c826b",
+    "url": "/img/askefza.1c7daef5.png"
+  },
+  {
+    "revision": "531faa022a14d47de0a37f4154970c08",
+    "url": "/img/back-icon.531faa02.svg"
+  },
+  {
+    "revision": "7b9282c4dae7eaa1b1d65a3055da1942",
+    "url": "/img/basit-suzinak.7b9282c4.png"
+  },
+  {
+    "revision": "c58e685bcbf62e46d30141bce453dd05",
+    "url": "/img/hicazkar.c58e685b.png"
+  },
+  {
+    "revision": "873a6c353ce3a095a4c3b6d7ef623879",
+    "url": "/img/home-icon.873a6c35.svg"
+  },
+  {
+    "revision": "b05da6ce31ae5a93bc1760333cf4ca1a",
+    "url": "/img/karcigar.b05da6ce.png"
+  },
+  {
+    "revision": "551e61e4e8f6c54857f7fcb40711feee",
+    "url": "/img/kurdi.551e61e4.png"
+  },
+  {
+    "revision": "2394b415df84d5b34e0cdca2a818bc55",
+    "url": "/img/kurdilihicazkar.2394b415.png"
+  },
+  {
+    "revision": "a8c4a8cf72753e139556fe3448fad0e1",
+    "url": "/img/logo.a8c4a8cf.svg"
+  },
+  {
+    "revision": "4fb352dc25ee2810a9b69c0a3bec092e",
+    "url": "/img/note-icon.4fb352dc.svg"
+  },
+  {
+    "revision": "b2fa8de9e067d637ab3961c2704bdbb3",
+    "url": "/img/scale-icon.b2fa8de9.svg"
+  },
+  {
+    "revision": "26f32f89977fa42ad68796925a95f642",
+    "url": "/img/search-icon.26f32f89.svg"
+  },
+  {
+    "revision": "02fd11394607834601a99d141ab74bad",
+    "url": "/img/seddi-araban.02fd1139.png"
+  },
+  {
+    "revision": "c7b80296a8d9e74c5aa12eb40f12817b",
+    "url": "/img/sultaniyegah.c7b80296.png"
+  },
+  {
+    "revision": "1d2326faaa94f3e219eb20712ac0b4b0",
+    "url": "/img/suzi-dil.1d2326fa.png"
+  },
+  {
+    "revision": "b0327d9ff280dc15db8329001efd75ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22d7d664122cd6454bac",
+    "url": "/js/app.ec127d75.js"
+  },
+  {
+    "revision": "272049fc1c40f4915656",
+    "url": "/js/chunk-vendors.d7246671.js"
+  },
+  {
+    "revision": "afda3a44681c6d0eb3cd3fac7a7f63d3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
